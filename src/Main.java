2527fd2d37e9f221498306d30d2ec35dfcedf09f
Admin <anstdor@gmail.com>
2022-08-11 14:18:36 +0300

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        int[] array = new int[3];
+        double[] array2 = {1.57, 7.654, 9.986};
+        long[] array3 = new long[2];
+
+    }
+
+    // Задача 2
+
+    {
+        int[] array = {1, 2, 3};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+
+        }
+    }
+}
